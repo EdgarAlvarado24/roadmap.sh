3b2b8aa35f6f32b9977c561fb0fa2bc96cad0873
EdarAlvarado24 <edgar.alvarado.16@gmail.com>
2025-11-25 13:49:03 +0000

    readme task-tracker

--- task-tracker/script.js
+++ task-tracker/script.js
@@ -4,31 +4,31 @@ let inputCreateTask = document.querySelector('.create-task');
 
 let  taskList = [
     {
-        destription:"New Task is created and added to the list",
+        description:"New Task is created and added to the list",
         status:{
             completed:false
         },
     },
     {
-        destription:"Clicking the checkbox toggles the completeness",
+        description:"Clicking the checkbox toggles the completeness",
+        status:{
+            completed:true
+        },
+    },
+    {
+        description:"Delete button will delete from the list",
         status:{
             completed:false
         },
     },
     {
-        destription:"Delete button will delete from the list",
-        status:{
-            completed:false
-        },
-    },
-    {
-        destription:"Complete tasks show at the end  with strikethrough",
+        description:"Complete tasks show at the end  with strikethrough",
         status:{
             completed:false
         },
     },   
     {
-        destription:"Marking in complete will put it back in pending list",
+        description:"Marking in complete will put it back in pending list",
         status:{
             completed:false
         },
@@ -37,27 +37,41 @@ let  taskList = [
 
 
 taskList.forEach((task)=>{
-    createContainerListTask(task.destription);
+    createContainerListTask(task);
 })
 
 
 buttonCreateTask.addEventListener("click",()=>{
-    newTaskName = inputCreateTask.value
-    createContainerListTask(newTaskName)
+
+    newTask = {
+        description: inputCreateTask.value,
+        status: {
+            completed:false
+        },
+    }
+
+    Array(listTask).push(newTask)
+    createContainerListTask(newTask)
 })
 
 
-function createContainerListTask(name){
+function createContainerListTask(task){
     let containerListTask = document.createElement('div')
     let newCheckbox = document.createElement('input')
     let newTask = document.createElement('p')
     let newIcon = document.createElement('i')
+    let statusTaskCompleted = task.status.completed
     
     containerListTask.classList.add('container-list-task')
     newCheckbox.type = 'checkbox';
     newCheckbox.classList.add('grown-checkbox')
-    newTask.innerText = name;
+    newTask.innerText = task.description;
     newIcon.classList.add('bi-trash-fill','delete-button')
+
+    if(statusTaskCompleted == true){
+        newTask.classList.add('completed-task');
+        newCheckbox.setAttribute('checked','checked')
+    }
     // console.log(inputCreateTask.value)
     containerListTask.appendChild(newCheckbox);
     containerListTask.appendChild(newTask);
@@ -67,16 +81,25 @@ function createContainerListTask(name){
 }
 
 
-
-// console.log(containerList.children)
-
 listTask = containerList.children;
 
-for(let i = 0; i < listTask.length; i++){
-    console.log(listTask[i].lastChild)
-    listTask[i].lastChild.addEventListener("click", ()=>{
+for(let i = 0; i <= listTask.length - 1; i++){
+    listTask[i].lastChild.addEventListener("click", (event)=>{
     // remover este elemento de la lista 
-    console.log('click');
+    containerList.removeChild(listTask[i])
    })
+}
 
+for(let i = 0; i <= listTask.length - 1; i++){
+    listTask[i].firstChild.addEventListener("click", (event =>{
+        taskText = listTask[i].children[1]
+        if(!taskText.classList.contains('completed-task')){
+            taskText.classList.add('completed-task')
+            console.log(listTask[i])
+            // taskList[i].firstChild.setAttribute('checked','checked')
+        }else{
+            taskText.classList.remove('completed-task')
+        }
+        // console.log(taskText)
+    }))
 }
